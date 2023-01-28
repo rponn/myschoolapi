ed976c587d29dfa8ac5cf6cd9cd5922541c464bb
rponn <rponn@users.noreply.github.com>
2023-01-28 14:22:28 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
-    Systen.out.println("development");
+    System.out.println("development");
+    System.out.println("Release");
   }
 }
